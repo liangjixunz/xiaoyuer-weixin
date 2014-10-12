66ba5020b539df40e0ad5b49f2d392d018d9f950
renaesop <njurjs@gmail.com>
2014-10-12 14:07:36 +0800

    使用闭包优化了校验函数

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ app.use(express.cookieSession());
 app.use(app.router);
 
 
+
 app.use(express.query());
 app.use('/wechat', wechat('weixin').text(function (message, req, res, next) {
          res.reply(routes.reply.auto_reply(message.Content));
@@ -38,7 +39,9 @@ app.use('/wechat', wechat('weixin').text(function (message, req, res, next) {
     })
 }).middlewarify());
 
-
+/*
+*对于自动回复的修改和查看
+ */
 app.get("/reply/get/auto",routes.reply.get_auto_reply);
 app.get("/reply/get/sub",routes.reply.get_sub_reply);
 app.post("/reply/set/append",routes.reply.append);
@@ -46,8 +49,29 @@ app.get("/reply/set/delete",routes.reply.delete);
 app.post("/reply/set/edit",routes.reply.edit);
 app.post("/reply/set/sub",routes.reply.edit_sub);
 
+
 /*
-*提供的apis
+ *使用api时的校验
+ */
+var signature_check = (function(){
+    var token = JSON.parse(fs.readdirSync(__dirname+"/../../shared/appConfig")).token;
+    var sha1 = crypto.createHash('sha1')
+    return function(req,res,next){
+        if(req.query.timestamp&req.query.check){
+            sha1.update("xye");
+            sha1.update(req.query.timestamp);
+            if(req.query.check==sha1.digest('hex')){
+                next();
+            }
+            else
+                res.send(JSON.stringify({code:"-10","err":"permission denied"}))
+        }
+        else
+            res.send(JSON.stringify({code:"-10","err":"permission denied"}))
+    }
+})();
+/*
+ *提供的apis
  */
 app.get("/qr",signature_check,routes.api.qr);
 app.post("/order/new",signature_check,routes.api.neworder);
@@ -55,18 +79,5 @@ app.post("/order/generate",signature_check,routes.api.orderSuccess);
 app.post("/order/ensure",signature_check,routes.api.ensure);
 
 
-function signature_check(req,res,next){
-    var sha1 = crypto.createHash('sha1')
-    if(req.query.timestamp&req.query.check){
-        sha1.update("xye");
-        sha1.update(req.query.timestamp);
-        if(req.query.check==sha1.digest('hex')){
-            next();
-        }
-        else
-            res.send(JSON.stringify({code:"-10","err":"permission denied"}))
-    }
-    else
-        res.send(JSON.stringify({code:"-10","err":"permission denied"}))
-}
+
 module.exports = app;
